de3adf1c781bda85c8d364ab748c45fc283bff6a
Hackbright Student <user@mini-005.(none)>
2014-02-18 12:26:37 -0800

    Moar rocks.

--- game.py
+++ game.py
@@ -11,8 +11,8 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
 class Rock(GameElement):
@@ -24,8 +24,20 @@ class Rock(GameElement):
 def initialize():
     """Put game initialization code here"""
     #we all want a rock to tie a piece of string around
-    rock = Rock()
-    GAME_BOARD.register(rock)
-    GAME_BOARD.set_el(2,2, rock)
-    print "The rock is at", (rock.x , rock.y)
+    rock_positions = [
+        (2, 1),
+        (1, 2),
+        (3, 2),
+        (2, 3),
+        (4, 4)
+        ]
 
+    rocks = []
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
