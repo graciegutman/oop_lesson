d8c49d7ac63fec6a2c4428287ca4b90f25f17fd0
Hackbright Student <user@mini-005.(none)>
2014-02-18 12:00:40 -0800

    We have a rock! Rocking all night.

--- game.py
+++ game.py
@@ -15,9 +15,17 @@ GAME_WIDTH = 3
 GAME_HEIGHT = 3
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    #some shit here!
+    IMAGE = 'Rock'
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    #we all want a rock to tie a piece of string around
+    rock = Rock()
+    GAME_BOARD.register(rock)
+    GAME_BOARD.set_el(2,2, rock)
+    print "The rock is at", (rock.x , rock.y)
+
