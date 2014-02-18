340a9f06f51c89dd4fb91b231b0fb3675ac890b4
Hackbright Student <user@mini-005.(none)>
2014-02-18 15:55:04 -0800

    Finished setting up initial dynamics.

--- game.py
+++ game.py
@@ -11,16 +11,38 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 5
+GAME_WIDTH = 10
 GAME_HEIGHT = 5
 
 #### Put class definitions here ####
 class Rock(GameElement):
-    #some shit here!
     IMAGE = 'Rock'
+    SOLID = True
 
 class Character(GameElement):
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
     IMAGE = 'Horns'
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+
+class Gem(GameElement):
+    IMAGE = "BlueGem"
+    SOLID = False
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You got a gem! You have %d items!" %(len(player.inventory)))
 
 ####   End class definitions    ####
 
@@ -28,7 +50,7 @@ def initialize():
     """Put game initialization code here"""
     #we all want a rock to tie a piece of string around
     rock_positions = [
-        (2, 1),
+        (0, 0),
         (1, 2),
         (3, 2),
         (2, 3),
@@ -48,29 +70,38 @@ def initialize():
     GAME_BOARD.set_el(2, 2, PLAYER)
     print PLAYER
 
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
+
     GAME_BOARD.draw_msg("Your message here for the low low price of one human child!")
 
 def keyboard_handler():
-    if KEYBOARD[key.UP]:
-        GAME_BOARD.draw_msg("You pressed up")
-        next_y = PLAYER.y - 1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
-    elif KEYBOARD[key.DOWN]:
-        GAME_BOARD.draw_msg("You pressed down")
-        next_y = PLAYER.y + 1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
-    elif KEYBOARD[key.RIGHT]:
-        GAME_BOARD.draw_msg("You pressed right")
-        next_x = PLAYER.x + 1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
-    elif KEYBOARD[key.LEFT]:
-        GAME_BOARD.draw_msg("You pressed wrong. loljk left.")
-        next_x = PLAYER.x - 1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
-    elif KEYBOARD[key.SPACE]:
-        GAME_BOARD.erase_msg()
+    direction = None
 
+    if KEYBOARD[key.UP]:
+        direction = "up"
+    if KEYBOARD[key.DOWN]:
+        direction = "down"
+    if KEYBOARD[key.LEFT]:
+        direction = "left"
+    if KEYBOARD[key.RIGHT]:
+        direction = "right"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        x_range = range(GAME_WIDTH)
+        y_range = range(GAME_HEIGHT)
+
+        if next_x in x_range and next_y in y_range:
+
+            existing_el = GAME_BOARD.get_el(next_x, next_y)
+            if existing_el:
+                existing_el.interact(PLAYER)
+
+            if existing_el is None or not existing_el.SOLID:
+                GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+                GAME_BOARD.set_el(next_x, next_y, PLAYER)
