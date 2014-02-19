756684c1958b710afa2c08a8522f5a65b6d961c5
Hackbright Student <user@mini-005.(none)>
2014-02-19 12:13:59 -0800

    Now with 2 players

--- game.py
+++ game.py
@@ -11,8 +11,8 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 10
-GAME_HEIGHT = 5
+GAME_WIDTH = 6
+GAME_HEIGHT = 10
 
 #### Put class definitions here ####
 class Rock(GameElement):
@@ -24,10 +24,6 @@ class Character(GameElement):
         GameElement.__init__(self)
         self.inventory = []
 
-    curr_position = 0 
-    IMG_list = ['Cat', 'Girl', 'Horns', 'Princess', 'Boy', 'Rock']
-    IMAGE = IMG_list[curr_position]
-
     def next_pos(self, direction):
         if direction == "up":
             return (self.x, self.y-1)
@@ -38,13 +34,6 @@ class Character(GameElement):
         elif direction == "right":
             return (self.x+1, self.y)
         return None
-
-    def change_char(self):
-        print "YAYE I speeleeed things wronnnng"
-        self.IMAGE = self.IMG_list[self.curr_position % len(self.IMG_list)]
-        GAME_BOARD.register(self)
-        self.curr_position += 1
-
 
 class Gem(GameElement):
     IMAGE = "BlueGem"
@@ -73,11 +62,15 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
-    global PLAYER
-    PLAYER = Character()
-    GAME_BOARD.register(PLAYER)
-    GAME_BOARD.set_el(2, 2, PLAYER)
-    print PLAYER
+    global P1
+    P1 = Character()
+    GAME_BOARD.register(P1)
+    GAME_BOARD.set_el(0, GAME_HEIGHT-1, P1)
+
+    global P2
+    P2 = Character()
+    GAME_BOARD.register(P2)
+    GAME_BOARD.set_el(GAME_WIDTH-1, GAME_HEIGHT-1, P2)
 
     gem = Gem()
     GAME_BOARD.register(gem)
@@ -86,23 +79,32 @@ def initialize():
     GAME_BOARD.draw_msg("Your message here for the low low price of one human child!")
 
 def keyboard_handler():
-    if KEYBOARD[key.SPACE]:
-        print "Yay! TOAST~!"
-        PLAYER.change_char()
+    direction1 = None
+    direction2 = None
 
-    direction = None
-
+    #p1 keys
     if KEYBOARD[key.UP]:
-        direction = "up"
+        direction1 = "up"
     if KEYBOARD[key.DOWN]:
-        direction = "down"
+        direction1 = "down"
     if KEYBOARD[key.LEFT]:
-        direction = "left"
+        direction1 = "left"
     if KEYBOARD[key.RIGHT]:
-        direction = "right"
+        direction1 = "right"
 
-    if direction:
-        next_location = PLAYER.next_pos(direction)
+    #p2 keys
+    if KEYBOARD[key.W]:
+        direction2 = "up"
+    if KEYBOARD[key.S]:
+        direction2 = "down"
+    if KEYBOARD[key.A]:
+        direction2 = "left"
+    if KEYBOARD[key.D]:
+        direction2 = "right"
+
+
+    if direction1:
+        next_location = P1.next_pos(direction1)
         next_x = next_location[0]
         next_y = next_location[1]
 
@@ -113,8 +115,25 @@ def keyboard_handler():
 
             existing_el = GAME_BOARD.get_el(next_x, next_y)
             if existing_el:
-                existing_el.interact(PLAYER)
+                existing_el.interact(P1)
 
             if existing_el is None or not existing_el.SOLID:
-                GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-                GAME_BOARD.set_el(next_x, next_y, PLAYER)
+                GAME_BOARD.del_el(P1.x, P1.y)
+                GAME_BOARD.set_el(next_x, next_y, P1)
+    if direction2:
+        next_location = P2.next_pos(direction2)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        x_range = range(GAME_WIDTH)
+        y_range = range(GAME_HEIGHT)
+
+        if next_x in x_range and next_y in y_range:
+
+            existing_el = GAME_BOARD.get_el(next_x, next_y)
+            if existing_el:
+                existing_el.interact(P2)
+
+            if existing_el is None or not existing_el.SOLID:
+                GAME_BOARD.del_el(P2.x, P2.y)
+                GAME_BOARD.set_el(next_x, next_y, P2)
